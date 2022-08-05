6127d2b4c76c4b1e5207f079a22e7954703865cf
rwambui <rosemwangi305@gmail.com>
2022-08-05 13:23:48 +0300

    wrap the app with themeProvider

--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@ import React from 'react';
 import {useState,useEffect} from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products';
-import {commerce } from './lib/commerce';
-
+import {commerce} from './lib/commerce';
+import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import {orange, red} from '@material-ui/core/colors';
+import 'fontsource-roboto';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: orange[400],
+      },
+      secondary: {
+        main: red[400],
+      }
+    }
+  });
 
   const fetchProducts = async() => {
     const { data } = await commerce.products.list();
     setProducts(data);
   }
+  const fetchCart = async() =>{
+    const { cart } = await commerce.cart.retrieve()
+    setCart(cart);
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
 
-  console.log(products);
+  // console.log(products);
+  console.log(cart);
   return (
     <div>
-      <Navbar/>
-      <Products products={products}/>
+      <ThemeProvider theme={theme}>
+       <Navbar />      
+       <Products products={products}/>
+      </ThemeProvider>
+
     </div>
   )
 }
