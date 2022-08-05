645e87bdb6d90b7b3e0eef887a21a76900d95cbb
rwambui <rosemwangi305@gmail.com>
2022-08-05 13:22:36 +0300

    adding typography and theme

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,43 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge,Typography } from '@material-ui/core'
-import { ShoppingCart } from '@material-ui/icons'
-import logo from '../../assets/images/Navicon.jpg';
+import { AppBar, Toolbar, IconButton,Badge,Typography,Button } from '@material-ui/core'
+import MenuIcon from "@material-ui/icons/Menu";
+import { ShoppingCart} from '@material-ui/icons'
 import useStyles from './styles';
 
 const Navbar = () => {
   const classes = useStyles();
   return (
     <>
-      <AppBar  position='fixed' className={classes.appBar} color='inherit'>
+      <AppBar  position='fixed' className={classes.appBar} color='primary'>
         <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit">
-            <img src={logo} alt=" Bobo Collection Store" height="25px" className={classes.image} />
-            Bobo Collection Store
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title} color="inherit">
+            {/* <MenuIcon/> */}
+            Bobo Collection Store            
           </Typography>
+          <Typography variant="h6" className={classes.title} color="inherit">
+          Category       
+          </Typography>         
+
+          <Typography variant="h6" className={classes.title} color="inherit">
+                Search       
+          </Typography>          
           <div className={classes.grow}/>
-          <div className={classes.button}>
+          <div className={classes.button}>            
             <IconButton aria-label="show cart items" color="inherit">
               <Badge badgeContent={2} color="secondary">
                 <ShoppingCart/>
               </Badge>
             </IconButton>
           </div>
+          <Button>
+              Login
+            </Button>
         </Toolbar>
       </AppBar>
     </>
   )
 }
-
 export default Navbar
