3c8bbf0a8a1e85ff6abb773851294ff165a1fb8b
rwambui <rosemwangi305@gmail.com>
2022-08-11 14:41:36 +0300

    dynamically distinguishing navbar list items

---
+++ src/components/NavItems/NavItems.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box, Button } from "@material-ui/core";
+
+const pages = ["Category", "Products", "Pricing", "Search"];
+// const settings = ["Account", "Logout"];
+const Menubar = () => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+        {pages.map((page) => (
+          <Button key={page} sx={{ my: 2, color: "white", display: "block" }}>
+            {page}
+          </Button>
+        ))}
+      </Box>
+    </>
+  );
+};
+export default Menubar;
