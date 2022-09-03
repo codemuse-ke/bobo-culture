ee7bb5ad853efab77caeccf3e1848f50184c9ab8
rwambui <rosemwangi305@gmail.com>
2022-09-03 18:56:15 +0300

    added classes for styling

---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(() => ({
+  media: {
+    height: 250,
+  },
+  content: {
+    justifyContent: "space-between",
+    display: "flex",
+  },
+  actions: {
+    justifyContent: "space bwtween",
+  },
+  buttons: {
+    display: "flex",
+    alignItems: "center",
+  },
+}));
