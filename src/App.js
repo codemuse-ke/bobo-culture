5a617b1fe1908fb2a86abb5c6536e1d169a5a3e7
rwambui <rosemwangi305@gmail.com>
2022-09-15 13:39:23 +0300

    Implemented functionalities for updating cart items

--- src/App.js
+++ src/App.js
@@ -33,8 +33,21 @@ const App = () => {
   };
 
   const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
+    const { cart } = await commerce.cart.add(productId, quantity);
+    setCart(cart);
+  };
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart);
+  };
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  };
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
   };
 
   useEffect(() => {
@@ -42,13 +55,11 @@ const App = () => {
     fetchCart();
   }, []);
 
-  console.log(cart);
-  console.log(cart.total_items);
   return (
     <div>
       <ThemeProvider theme={theme}>
-        {/* <Navbar totalItems={cart?.total_items} /> */}
         <Router>
+          <Navbar totalItems={cart?.total_items} />
           <Routes>
             <Route
               path="/"
@@ -56,7 +67,17 @@ const App = () => {
                 <Products products={products} onAddToCart={handleAddToCart} />
               }
             />
-            <Route path="/cart" element={<Cart cart={cart} />} />
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  cart={cart}
+                  handleUpdateCartQty={handleUpdateCartQty}
+                  handleRemoveFromCart={handleRemoveFromCart}
+                  handleEmptyCart={handleEmptyCart}
+                />
+              }
+            />
             <Route
               path="*"
               element={
