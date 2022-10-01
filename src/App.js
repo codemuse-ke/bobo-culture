718fe935fd30d15f17607b8c0269d2623df37bf6
Rose <rose@codemuse.local>
2022-10-01 22:47:54 +0300

    corrected display bug

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import { Products, Navbar, Cart, Checkout } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
